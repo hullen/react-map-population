2f47539d9366d9d4bc18db0d42452f4e0e68384a
Hullen Gonzales <hullenn@gmail.com>
2020-08-17 23:43:26 -0300

    Updates

---
+++ precache-manifest.d65736c64955f81e17466966b145a15a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b723d7d390d16e264e77ee06d7af7ea",
+    "url": "https://hullen.github.io/react-map-population/index.html"
+  },
+  {
+    "revision": "c735aaf251b22896bf96",
+    "url": "https://hullen.github.io/react-map-population/static/css/3.b4a16772.chunk.css"
+  },
+  {
+    "revision": "333de600f611774f1b31",
+    "url": "https://hullen.github.io/react-map-population/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f09e6827d98dce61266b",
+    "url": "https://hullen.github.io/react-map-population/static/js/0.6c0caa01.chunk.js"
+  },
+  {
+    "revision": "c735aaf251b22896bf96",
+    "url": "https://hullen.github.io/react-map-population/static/js/3.c191ecd5.chunk.js"
+  },
+  {
+    "revision": "9afb665a861ae8d954c2ee41a6a2e856",
+    "url": "https://hullen.github.io/react-map-population/static/js/3.c191ecd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74df2dfb3a2901a45318",
+    "url": "https://hullen.github.io/react-map-population/static/js/4.44010b76.chunk.js"
+  },
+  {
+    "revision": "1cbaf067f252a9e5d9b7",
+    "url": "https://hullen.github.io/react-map-population/static/js/5.16f82819.chunk.js"
+  },
+  {
+    "revision": "afe01e95649f473f825c",
+    "url": "https://hullen.github.io/react-map-population/static/js/6.a8c7d5de.chunk.js"
+  },
+  {
+    "revision": "cbc26891e23d284a1efd",
+    "url": "https://hullen.github.io/react-map-population/static/js/7.96c30adc.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "https://hullen.github.io/react-map-population/static/js/7.96c30adc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "333de600f611774f1b31",
+    "url": "https://hullen.github.io/react-map-population/static/js/main.4630a966.chunk.js"
+  },
+  {
+    "revision": "ef4544714619fbebd7c2",
+    "url": "https://hullen.github.io/react-map-population/static/js/runtime-main.d5289acf.js"
+  },
+  {
+    "revision": "5cc1f57c14b8f14702875b132977f3ff",
+    "url": "https://hullen.github.io/react-map-population/static/media/property-icon.5cc1f57c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-map-population/precache-manifest.b84f5529794607c81a7693c55261618e.js"
+  "https://hullen.github.io/react-map-population/precache-manifest.d65736c64955f81e17466966b145a15a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-map-population/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://hullen.github.io/react-map-population/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
