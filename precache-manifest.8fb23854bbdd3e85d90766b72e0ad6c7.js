1664e27145eec551a5545facce24c738227d8d6b
Hullen Gonzales <hullenn@gmail.com>
2022-10-26 20:28:11 -0300

    Updates

---
+++ precache-manifest.8fb23854bbdd3e85d90766b72e0ad6c7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05f74b0db2d493bdb8b3e8d14f18c6ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c02dd69e6ebe47b3e3c",
+    "url": "/static/css/3.b4a16772.chunk.css"
+  },
+  {
+    "revision": "8b51d6a248c889f7777d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f09e6827d98dce61266b",
+    "url": "/static/js/0.6c0caa01.chunk.js"
+  },
+  {
+    "revision": "5c02dd69e6ebe47b3e3c",
+    "url": "/static/js/3.5e539d58.chunk.js"
+  },
+  {
+    "revision": "9afb665a861ae8d954c2ee41a6a2e856",
+    "url": "/static/js/3.5e539d58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22cca2517ce4649754e7",
+    "url": "/static/js/4.9657263b.chunk.js"
+  },
+  {
+    "revision": "27e62bda1059a6656acb",
+    "url": "/static/js/5.3b68fab1.chunk.js"
+  },
+  {
+    "revision": "a466899725ee8bc1551d",
+    "url": "/static/js/6.ac7f4ea2.chunk.js"
+  },
+  {
+    "revision": "6da45677b177f2ec259b",
+    "url": "/static/js/7.f8727ca6.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/static/js/7.f8727ca6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b51d6a248c889f7777d",
+    "url": "/static/js/main.9e0a1d01.chunk.js"
+  },
+  {
+    "revision": "066fbc2760e5282d2c42",
+    "url": "/static/js/runtime-main.cc2fde04.js"
+  },
+  {
+    "revision": "5cc1f57c14b8f14702875b132977f3ff",
+    "url": "/static/media/property-icon.5cc1f57c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-map-population/precache-manifest.3874abe064ed3e152faabdcab59f0a33.js"
+  "/precache-manifest.8fb23854bbdd3e85d90766b72e0ad6c7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-map-population/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
